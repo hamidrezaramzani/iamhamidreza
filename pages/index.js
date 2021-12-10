07d7b729424d922ff1b033796110f45a0d6d55b9
Hamidreza <hrra13801396h@gmail.com>
2021-12-10 21:13:28 +0330

    Add responsive meta tag

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ export default function Home() {
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png"/>
         <meta name="theme-color" content="#ffffff"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       <Header />
