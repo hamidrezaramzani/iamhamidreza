5e8089c781f9c220fb6d98d601cefdc0d6e9e088
Hamidreza <hrra13801396h@gmail.com>
2021-12-13 20:19:14 +0330

    Remove pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
-
-module.exports = withPWA({
+module.exports = {
   reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    runtimeCaching,
-  },
-});
+}
--- pages/api/blog/all.js
+++ pages/api/blog/all.js
@@ -1,7 +1,7 @@
 const db = require("../../../lib/database");
 export default function handle(req, res) {
   db.query("SELECT * FROM blogs", (err, result) => {
-    if (err) res.status(400).json({ message: "we have an error on server" });
+    if (err) res.status(400).json(err);
 
     const data = JSON.parse(JSON.stringify(result));
     res.status(200).json(data);
