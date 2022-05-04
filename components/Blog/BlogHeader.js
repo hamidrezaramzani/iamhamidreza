a8c3047d840e89f69af22798a50258e0e16330d7
Hamidreza <hrra13801396h@gmail.com>
2022-05-04 23:33:21 +0430

    Fix clipboard bug in chrome browsers

--- components/Blog/BlogHeader.js
+++ components/Blog/BlogHeader.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { header, blog_info, blog_image } from "./styles.module.css";
-import {MdOutlineContentCopy} from "react-icons/md";
+import { MdOutlineContentCopy } from "react-icons/md";
 import swal from "sweetalert2";
-function BlogHeader({ title, description, image , link , domain }) {
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+function BlogHeader({ title, description, image, link, domain }) {
 
-  const handleClickCopyUrl = () => {
-    navigator.clipboard.writeText(`${domain}/b/${link}`);
+  const handleOnCopy = () => {
     swal.fire({
-      title :"Copied" , 
-      text : "this blog url is copied" , 
-      icon : "info"
+      title: "Copied",
+      text: "this blog url is copied",
+      icon: "info"
     })
   }
   return (
     <div className={header}>
       <div className={blog_info}>
-        <h1>{title}  <span>{}</span></h1>
+        <h1>{title}  <span>{ }</span></h1>
         <p>{description}</p>
-        <button onClick={handleClickCopyUrl}>{link} <MdOutlineContentCopy fontSize={18} /></button>
+        <CopyToClipboard text={`${domain}/b/${link}`}
+          onCopy={handleOnCopy}>
+          <button>{link} <MdOutlineContentCopy fontSize={18} /></button>
+        </CopyToClipboard>
+
       </div>
       <div className={blog_image} >
         <img src={image} width={300} alt={description} />
